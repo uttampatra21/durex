620387862f5402e82a062691296ffeccd154afbe
uttam kumar patra <dev.uttampatra@gmail.com>
2023-11-27 20:01:16 +0530

    0

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -89,33 +89,6 @@ const Dashboard = () => {
                 </table>
               </div>
 
-              {/* <!-- Reminders --> */}
-              <div class="reminders">
-                <div class="header">
-                  <i class="bx bx-note"></i>
-                  <h3>Remiders</h3>
-                  <i class="bx bx-filter"></i>
-                  <i class="bx bx-plus"></i>
-                </div>
-                <ul class="task-list">
-                  <li class="completed">
-                    <div class="task-title">
-                      <i class="bx bx-check-circle"></i>
-                      <p>Complete Your Task</p>
-                    </div>
-                    <i class="bx bx-dots-vertical-rounded"></i>
-                  </li>
-                  <li class="completed">
-                    <div class="task-title">
-                      <i class="bx bx-check-circle"></i>
-                      <p>Analyse Our Site</p>
-                    </div>
-                    <i class="bx bx-dots-vertical-rounded"></i>
-                  </li>
-                </ul>
-              </div>
-
-              {/* <!-- End of Reminders--> */}
             </div>
           </main>
         </div>
