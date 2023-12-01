bbacbf65651440791cc6de4bec25489bf6a20fdf
uttam kumar patra <dev.uttampatra@gmail.com>
2023-12-01 13:15:41 +0530

    0

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Service.css";
 const Service = () => {
-  return <div id="service"></div>;
+  return (
+    <div id="service" className="">
+      <img
+        width="100%"
+        src="https://github.com/uttampatra21/public/raw/main/p3.jpg"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Service;
